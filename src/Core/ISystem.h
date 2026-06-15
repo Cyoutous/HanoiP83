0214dbcba03b3e93c91b1b70d6d97b07ded92586
Cyoutous <60254441+Cyoutous@users.noreply.github.com>
2026-06-15 19:44:14 +0800

    feat: 调整了系统接口的定义，适应后续架构

--- src/Core/ISystem.h
+++ src/Core/ISystem.h
@@ -9,15 +9,29 @@ namespace entt {
 }
 class Resource;
 
+enum Phase {
+    Logic, Render
+}; //逻辑帧执行或者渲染帧执行
+
 class ISystem {
+
+    bool enabled_status = false;
+
 public:
+
     virtual ~ISystem() = default;
-    virtual std::string_view name() const = 0;
-    virtual std::vector<std::string_view> scenes() const { return {"gameplay"}; }
+
+    virtual std::string_view name() const = 0; //系统名称
+
+    virtual std::vector<std::string_view> scenes() const { return {"gameplay"}; } //所属游戏场景
+    virtual Phase phase() const { return Phase::Logic; } //执行时机
 
     virtual void on_start(entt::registry& reg, Resource& res) {};
     virtual void on_update(entt::registry& reg, Resource& res) = 0;
     virtual void on_stop(entt::registry& reg, Resource& res) {};
+
+    bool enabled() const { return enabled_status; }
+    void set_enabled(bool value) { enabled_status = value; }
 };
 
 #endif
