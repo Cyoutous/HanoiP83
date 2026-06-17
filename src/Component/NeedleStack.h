a4d3aaf243170bb55c252c29fda161563a8b7a59
Cyoutous <60254441+Cyoutous@users.noreply.github.com>
2026-06-17 16:59:18 +0800

    feat: 新增了一些组件

--- src/Component/NeedleStack.h
+++ src/Component/NeedleStack.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <vector>
-#include <entt/entt.hpp>
+#include <entt/entity/fwd.hpp>
 
 struct NeedleStack {
     std::vector<entt::entity> disks;  // 从底到顶
---
+++ src/Component/NeedleTag.h
@@ -0,0 +1,5 @@
+#pragma once
+
+struct NeedleTag {
+    int index = 0;  // 0=左, 1=中, 2=右
+};
