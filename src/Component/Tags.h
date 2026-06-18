db99aa6a32d61872ce656b09d15d3e869a0e6947
Cyoutous <60254441+Cyoutous@users.noreply.github.com>
2026-06-18 15:05:42 +0800

    feat: 新增了若干按钮相关的标记组件

--- src/Component/Tags.h
+++ src/Component/Tags.h
@@ -6,3 +6,14 @@ struct NextAutoDemoTag{ };
 struct StepCounterTag { };
 struct BestCounterTag { };
 struct DiskCountTag { };
+
+struct ResetButtonTag {};
+struct AutoDemoButtonTag {};
+struct DiskCountArrowUpTag {};    // 右箭头，数量 +1
+struct DiskCountArrowDownTag {};  // 左箭头，数量 -1
+struct HistoryPanelToggleTag {};
+struct SettingsPanelToggleTag {};
+struct PanelCloseTag     {};
+struct SettingsConfirmTag {};
+struct SettingsCancelTag {};
+struct CloseButtonTag    {};
